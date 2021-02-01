dea87169581cb2b2d687a6048448ee5f8df2ffc8
HP01\Gregor <gregor.pirs@fri.uni-lj.si>
2021-02-01 17:05:53 +0100

    Added feature-a file.

---
+++ test3.cpp
@@ -0,0 +1,11 @@
+// test.cpp. Gregor Pirs
+// Description: Playing with Git
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello world!" << endl;
+  cout << "Hello 2!" << endl;
+}
